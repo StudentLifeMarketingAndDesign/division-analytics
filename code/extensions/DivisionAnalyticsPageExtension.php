b370c886e21e52cefc7b797e9a93626a4fe7bafc
Dustin Quam <quamsta@gmail.com>
2017-09-18 16:59:29 -0500

    removed debugging statement

--- code/extensions/DivisionAnalyticsPageExtension.php
+++ code/extensions/DivisionAnalyticsPageExtension.php
@@ -1,16 +1,13 @@
 <?php
 class DivisionAnalyticsPageExtension extends Extension {
 
-
 	public function Analytics(){
-		
 		if(Director::isLive()){
 			$config = SiteConfig::current_site_config(); 
 			$data = new ArrayData(array(
 				'GoogleAnalyticsID' => $config->GoogleAnalyticsID,
 				'UITrackingID' => $config->UITrackingID
-			));
-			echo 'hello';			
+			));			
 			return $data->renderWith('DivisionAnalytics');
 		}
 	}
