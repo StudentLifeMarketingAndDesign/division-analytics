d48b20c0f9661be7c5f51fd1ac4fe3b0cccff475
Dustin Quam <quamsta@gmail.com>
2020-10-20 12:57:58 -0500

    rolling out stable site identifier

--- code/extensions/DivisionAnalyticsPageControllerExtension.php
+++ code/extensions/DivisionAnalyticsPageControllerExtension.php
@@ -1,18 +1,18 @@
 <?php
 use SilverStripe\Core\Extension;
-use SilverStripe\Control\Director;
 use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\View\ArrayData;
-	class DivisionAnalyticsPageControllerExtension extends Extension {
-		
-		public function Analytics(){
-			if(Director::isLive()){
-				$config = SiteConfig::current_site_config(); 
-				$data = new ArrayData(array(
-					'GoogleAnalyticsID' => $config->GoogleAnalyticsID,
-					'UITrackingID' => $config->UITrackingID
-				));			
-				return $data->renderWith('DivisionAnalytics');
-			}
-		}
+
+class DivisionAnalyticsPageControllerExtension extends Extension {
+
+	public function Analytics() {
+
+		$config = SiteConfig::current_site_config();
+		$data = new ArrayData(array(
+			'GoogleAnalyticsID' => $config->GoogleAnalyticsID,
+			'UITrackingID' => $config->UITrackingID,
+		));
+		return $data->renderWith('DivisionAnalytics');
+
 	}
+}
--- code/extensions/DivisionAnalyticsSiteConfig.php
+++ code/extensions/DivisionAnalyticsSiteConfig.php
@@ -1,8 +1,8 @@
 <?php
-use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
-use SilverStripe\Forms\CheckboxField;
+use SilverStripe\ORM\DataExtension;
 use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\View\Parsers\URLSegmentFilter;
 
@@ -11,6 +11,7 @@ class DivisionAnalyticsSiteConfig extends DataExtension {
 	private static $db = array(
 		'GoogleAnalyticsID' => 'Text',
 		'DisableUITracking' => 'Boolean',
+		'InternalAnalyticsID' => 'Text',
 	);
 
 	private static $has_one = array(
@@ -25,21 +26,34 @@ class DivisionAnalyticsSiteConfig extends DataExtension {
 		$fields->addFieldToTab('Root.Analytics', new TextField('GoogleAnalyticsID', 'Google Analytics ID (UA-XXXXX-X)'));
 		$fields->addFieldToTab('Root.Analytics', new CheckboxField('DisableUITracking', 'Disable UI Tracking Utility'));
 
-
 		return $fields;
 	}
 
-	public function UITrackingID(){
-		$config = SiteConfig::current_site_config(); 
-
+	public function UITrackingID() {
+		$siteName = $this->InternalAnalyticsID;
 		$prefix = 'uiowa.edu.md-';
-		$filter = URLSegmentFilter::create();
-		$siteName = $config->Title;
-
-		$filteredSiteName = $filter->filter($siteName);
-
-		return $prefix.$filteredSiteName;
+		$filteredSiteName = $this->generateNiceSiteName($siteName);
+		return $prefix . $filteredSiteName;
 
 	}
 
+	public function generateNiceSiteName($name) {
+		$filter = URLSegmentFilter::create();
+		$filteredSiteName = $filter->filter($name);
+
+		return $filteredSiteName;
+	}
+
+	public function requireDefaultRecords() {
+		parent::requireDefaultRecords();
+
+		$config = SiteConfig::current_site_config();
+		$siteName = $config->Title;
+
+		if (!$config->InternalAnalyticsID) {
+			$config->InternalAnalyticsID = $this->generateNiceSiteName($siteName);
+			$config->write();
+		}
+
+	}
 }
