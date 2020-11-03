8b2bca8e89c5c9eceeb0c775b0b0008f55ca73ed
Dustin Quam <quamsta@gmail.com>
2020-11-03 08:46:52 -0600

    removed ui tracking id ref

--- code/extensions/DivisionAnalyticsPageControllerExtension.php
+++ code/extensions/DivisionAnalyticsPageControllerExtension.php
@@ -11,7 +11,6 @@ class DivisionAnalyticsPageControllerExtension extends Extension {
 			$config = SiteConfig::current_site_config();
 			$data = new ArrayData(array(
 				'GoogleAnalyticsID' => $config->GoogleAnalyticsID,
-				'UITrackingID' => $config->UITrackingID(),
 			));
 			return $data->renderWith('DivisionAnalytics');
 		}
